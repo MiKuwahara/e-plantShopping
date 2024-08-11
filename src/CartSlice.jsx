540fc1ecf0169c1a32a34a14a5d1b48c5db74e3c
Michelle Kuwahara <mxk200037@utdallas.edu>
2024-08-10 22:49:54 -0500

    Define reducer functions in CartSlice for adding, removing and updating a product. These functions are then exported so they can be used in other components such as in product listing and in cart as action creators. Lastly, connect cart slice reducer to the store, so components can access the reducer functions in cart slice.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+
 export const CartSlice = createSlice({
     name: 'cart',
     initialState: {
@@ -7,12 +8,25 @@ export const CartSlice = createSlice({
     },
     reducers: {
         addItem: (state, action) => {
-        
+            // Deconstruct
+            const {name, image, cost} = action.payload;
+            const existingItem = state.items.find(item => item.name === name);
+            if(existingItem){
+                existingItem.quantity += 1;
+            }else {
+                items.push({name, image, cost, quantity:1});
+            }
         },
         removeItem: (state, action) => {
+            state.items = state.items.filter(item => item.name !== action.payload.name);
         },
         updateQuantity: (state, action) => {
-
+            // Deconstruct 
+            const {name, quantity} = action.payload;
+            const itemToUpdate = state.items.find(item => item.name === name);
+            if(itemToUpdate){
+                itemToUpdate.quantity = quantity;
+            }
         
         },
     },
